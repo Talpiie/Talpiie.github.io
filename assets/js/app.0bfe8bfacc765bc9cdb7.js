4ac226166be553811d10b18f5fe23a6c168ac5a8
Rosh Asares <asaresrosh@gmail.com>
2024-10-21 23:10:16 +0800

    Update app.0bfe8bfacc765bc9cdb7.js

--- assets/js/app.0bfe8bfacc765bc9cdb7.js
+++ assets/js/app.0bfe8bfacc765bc9cdb7.js
@@ -1318,7 +1318,7 @@ function(e, t) {
             return n("header", {
                 staticClass: "section-header"
             }, [n("i", {
-                staticClass: "site-avatar"
+                staticClass: "site-avatar icn-erik"
             })])
         }]
     }
